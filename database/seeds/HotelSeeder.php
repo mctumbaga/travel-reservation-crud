de66a0aa3dedb5f360daea9fa3ced8e2102194b4
Your Name <you@example.com>
2020-02-07 00:14:49 +0800

    Added Database Seeders Namespace

--- database/seeds/HotelSeeder.php
+++ database/seeds/HotelSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Hotel;
 
 class HotelSeeder extends Seeder
 {
--- database/seeds/RoomSeeder.php
+++ database/seeds/RoomSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Room;
 
 class RoomSeeder extends Seeder
 {
