f4925f0fef79f48b0a80e55bd54e529a52289c9e
Your Name <you@example.com>
2020-02-06 23:35:10 +0800

    Added Routes, created Models and Database Migrations

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,17 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::view('/', 'home'); 
+Route::get('/hotels', 'HotelController@index');
+
+/* Auth0's Laravel package to handle authentication */
+Route::get('/auth0/callback', '\Auth0\Login\Auth0Controller@callback' )->name('auth0-callback');
+Route::get('/login', 'Auth\Auth0IndexController@login')->name('login');
+Route::get('/logout', 'Auth\Auth0IndexController@logout')->name('logout')->middleware('auth');
+
+/* For the dashboard */
+Route::group(['prefix' => 'dashboard'], function() {
+    Route::view('/', 'dashboard/dashboard');
+    Route::get('reservations/create/{id}', 'ReservationController@create');
+    Route::resource('reservations', 'ReservationController')->except('create');
 });
